805cc52106942e03c6d54ff62ae624d3c3a33863
papia <86232710+papiasharmin@users.noreply.github.com>
2023-03-30 01:10:35 +0900

    add calling

--- component/chat/header.js
+++ component/chat/header.js
@@ -1,9 +1,9 @@
 import React,{useEffect,useState,useContext, useRef} from 'react'
 import { gettime } from '../../helper'
-import  {Avatar,Badge,Button } from "../../node_modules/@mui/material";
+import  {Avatar,Badge,Button} from "../../node_modules/@mui/material";
 import Chattooltip from "./chatcontooltip";
 import classes from './chatcontainer.module.css'
-import {CloseOutlined,MoreHoriz, Notifications, Phone, VideoCall} from "@mui/icons-material";
+import {CloseOutlined,MoreHoriz, VideoCall,PhoneDisabled} from "@mui/icons-material";
 import { useRouter } from 'next/router';
 import Pushercontext from '../../pushercontext';
 
@@ -13,15 +13,13 @@ function Header({frienddata,user,deletemsg}) {
     let [ontooltip,setontooltip] = useState(false);
     let [time,settime] = useState(0);
     const router = useRouter()
-    const puserctx = useContext(Pushercontext)
-    const {  setName, callUser,callAccepted} = useContext(Pushercontext);
+    const { callUser,answerCall, callAccepted, userVideo, callEnded, call, leaveCall,} = useContext(Pushercontext);
+    let [showmyved,setshowmyved] = useState(false);
     const myVideo = useRef()
-    const friendVideo = useRef()
-    //puserctx.setmyvideo(myVideo);
-    //puserctx.setfriendvideo(friendVideo)
+   
     function handelMouseenter(event){    
         setshow(event?.currentTarget.id)  
-     };
+    };
      
      function handelMouseleave(){ 
        settime(setTimeout(()=>{    
@@ -54,44 +52,55 @@ function Header({frienddata,user,deletemsg}) {
     }
 
     // useEffect(()=>{
-    //   if(callAccepted && !myVideo.current.srcObject){
-    //     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    //       navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-    //         .then((currentStream) => {
-    //           setStream(currentStream);
-    //           console.log(currentStream)
-    //           myVideo.current.srcObject = currentStream
-    //           if(!stream){
-    //             myVideo.current.srcObject = stream
-    //           }
-    //       });
-    //       }
-
-
+    //   if(myvideostream && !myVideo.current){
+    //     myVideo.current.srcObject = myvideostream
     //   }
+    // },[myvideostream])
 
-    // },[callAccepted])
+    useEffect(()=>{
+      if(callEnded && myVideo.current){
+        setshowmyved(false)
+        myVideo.current.srcObject = null
+      }
+    },[callEnded])
 
-    function call(){
+    function answercall(){
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) { 
+        navigator.mediaDevices.getUserMedia({ audio: true })
+          .then((currentStream) => {
+            console.log(currentStream)
+            setshowmyved(true)
+            // if(myVideo.current.srcObject ){
+            //   myVideo.current.srcObject = currentStream
+            // }else{
+            //   myVideo.current.src = currentStream
+            // }
+            answerCall(currentStream)
+            myVideo.current.srcObject = currentStream
+        }).catch((error) =>{
+          console.log(error)
+        })
+        } 
+    }
+
+    function calluser(){
       
       if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         navigator.mediaDevices.getUserMedia({ video: true, audio: true })
           .then((currentStream) => {
-            //setStream(currentStream);
             console.log(currentStream)
-            setName(user.name)
-            callUser(frienddata._id,currentStream)
-            myVideo.current.srcObject = currentStream
-            // if(!stream){
-            //   myVideo.current.srcObject = stream
+            setshowmyved(true)
+            // if(myVideo.current.srcObject ){
+            //   myVideo.current.srcObject = currentStream
+            // }else{
+            //   myVideo.current.src = currentStream
             // }
+            myVideo.current.srcObject = currentStream
+            callUser(frienddata._id,currentStream)
+            
         });
-        }
-        
-       
+        }  
     }
-
-   
 
     const avatar =  <div className={classes.avatarbor}> 
                         <Badge className={frienddata.status == 'online'? classes.badge : classes.badgeoff} overlap="circular" variant="dot"></Badge>
@@ -108,12 +117,30 @@ function Header({frienddata,user,deletemsg}) {
             </div>
         </div>
         <div>
-            <VideoCall variant="contained" color="primary"  fullWidth onClick={call} />
+            <VideoCall variant="contained" color="primary"  sx={{ width :25, height:25}} onClick={calluser} />
             <MoreHoriz color="primary" id='tool' onMouseEnter={handelMouseenter} onMouseLeave={handelMouseleave}/> 
             {show === 'tool'  && <Chattooltip  onmouseenter={handeltooltipenter} onmouseleave={handeltooltipleave} user={user} friendemail={frienddata.email} deletemsg={deletemsg}/>}         
             <CloseOutlined sx={{ width :25, height:25}} color="primary" onClick={closechat}/>
         </div>
         <div className={classes.video}><video playsInline muted ref={myVideo} autoPlay  /></div>
+          {callAccepted && !callEnded && 
+  <div className={classes.callAccepted}>
+      <video playsInline ref={userVideo} muted autoPlay  />
+      <Button variant="contained" color="secondary" startIcon={<PhoneDisabled fontSize="large" />} fullWidth onClick={leaveCall} >
+          Hang Up
+      </Button>
+  </div>
+}
+         {call.isReceivingCall && !callAccepted && 
+<div className={classes.answercall} >
+    <h3>{call.name} is calling:</h3>
+    <Button variant="contained" color="primary" sx={{ width :50, height:25}} onClick={answercall}>
+        Answer
+    </Button>
+
+</div>
+}
+
         
     </header>
   )
